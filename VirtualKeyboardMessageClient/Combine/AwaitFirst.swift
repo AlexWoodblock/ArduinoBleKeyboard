72c9517f0f7398245f7528f80a26c2f1e09e5fa1
Alexander Leontev <alexwoodblock@icloud.com>
2023-06-18 02:33:08 +0200

    Fix awaitSink and awaitFirst not unsubscribing on task cancellation

--- VirtualKeyboardMessageClient/Combine/AwaitFirst.swift
+++ VirtualKeyboardMessageClient/Combine/AwaitFirst.swift
@@ -13,28 +13,42 @@ extension Publisher {
     /// Throws `NoElementError` if Publisher has completed before
     /// an emission was made.
     func awaitFirst() async throws -> Output {
+        // unfortunately, we can't simply clear cancellables
+        // due to their concurrent usage.
+        // Instead, we resort to a little trickery - this subject emission will signal the end of
+        // original upstream publisher, and we can safely signal it inside of cancellation handler
+        let forceCompletionSubject = PassthroughSubject<Void, Never>()
+        
         let upstream = self
         
         var cancellables = Set<AnyCancellable>()
         
         var didReceiveElement = false
         
-        return try await withCheckedThrowingContinuation { continuation in
-            upstream.first().eraseToAnyPublisher().sink { completion in
-                switch completion {
-                case .failure(let error):
-                    continuation.resume(throwing: error)
-                    
-                case .finished:
-                    if !didReceiveElement {
-                        continuation.resume(throwing: NoElementError())
-                    }
-                }
-            } receiveValue: { output in
-                didReceiveElement = true
-                
-                continuation.resume(returning: output)
-            }.store(in: &cancellables)
+        return try await withTaskCancellationHandler {
+            return try await withCheckedThrowingContinuation { continuation in
+                upstream
+                    .prefix(untilOutputFrom: forceCompletionSubject)
+                    .first()
+                    .eraseToAnyPublisher()
+                    .sink { completion in
+                        switch completion {
+                        case .failure(let error):
+                            continuation.resume(throwing: error)
+                            
+                        case .finished:
+                            if !didReceiveElement {
+                                continuation.resume(throwing: NoElementError())
+                            }
+                        }
+                    } receiveValue: { output in
+                        didReceiveElement = true
+                        
+                        continuation.resume(returning: output)
+                    }.store(in: &cancellables)
+            }
+        } onCancel: {
+            forceCompletionSubject.send(())
         }
     }
 }
--- VirtualKeyboardMessageClient/Combine/AwaitSink.swift
+++ VirtualKeyboardMessageClient/Combine/AwaitSink.swift
@@ -12,22 +12,35 @@ extension Publisher {
     /// Applies `sink` function to this Publisher, and suspends until
     /// the Publisher completes.
     func awaitSink(receiveValue: @escaping (Output) -> Void) async throws {
+        let upstream = self
+        
+        // unfortunately, we can't simply clear cancellables
+        // due to their concurrent usage.
+        // Instead, we resort to a little trickery - this subject emission will signal the end of
+        // original upstream publisher, and we can safely signal it inside of cancellation handler
+        let forceCompletionSubject = PassthroughSubject<Void, Never>()
+        
         var cancellables = Set<AnyCancellable>()
         
         // return is only needed here because otherwise Swift wouldn't
         // understand it's Void
-        return try await withCheckedThrowingContinuation { continuation in
-            sink { completion in
-                switch completion {
-                case .failure(let error):
-                    continuation.resume(throwing: error)
-                case .finished:
-                    continuation.resume()
-                }
-            } receiveValue: { output in
-                receiveValue(output)
-            }.store(in: &cancellables)
-
+        return try await withTaskCancellationHandler {
+            return try await withCheckedThrowingContinuation { continuation in
+                upstream
+                    .prefix(untilOutputFrom: forceCompletionSubject)
+                    .sink { completion in
+                        switch completion {
+                        case .failure(let error):
+                            continuation.resume(throwing: error)
+                        case .finished:
+                            continuation.resume()
+                        }
+                    } receiveValue: { output in
+                        receiveValue(output)
+                    }.store(in: &cancellables)
+            }
+        } onCancel: {
+            forceCompletionSubject.send(())
         }
     }
     
